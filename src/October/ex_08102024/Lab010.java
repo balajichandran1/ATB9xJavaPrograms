deedf0335915140c895b841e42a004c32e49f1cb
BalajiChandran <catchbaalaaji@gmail.com>
2024-10-15 17:54:56 +0530

    Updating some of the task exercises

--- src/October/ex_08102024/Lab010.java
+++ src/October/ex_08102024/Lab010.java
@@ -3,26 +3,6 @@ package October.ex_08102024;
 public class Lab010 {
     public static void main(String[] args) {
 
-        int table =13;
-        System.out.printf("%d x %d = %d ", table , 1 ,table*1);
-        System.out.println();
-        System.out.printf("%d x %d x %d ", table , 1 ,table*2);
-        System.out.println();
-        System.out.printf("%d x %d x %d ", table , 1 ,table*3);
-        System.out.println();
-        System.out.printf("%d x %d x %d ", table , 1 ,table*4);
-        System.out.println();
-        System.out.printf("%d x %d x %d ", table , 1 ,table*5);
-        System.out.println();
-        System.out.printf("%d x %d x %d ", table , 1 ,table*6);
-        System.out.println();
-        System.out.printf("%d x %d x %d ", table , 1 ,table*7);
-        System.out.println();
-        System.out.printf("%d x %d x %d ", table , 1 ,table*8);
-        System.out.println();
-        System.out.printf("%d x %d x %d ", table , 1 ,table*9);
-        System.out.println();
-        System.out.printf("%d x %d x %d ", table , 1 ,table*10);
 
 
     }
---
+++ src/October/ex_10102024/Lab011.java
@@ -0,0 +1,17 @@
+package October.ex_10102024;
+
+public class Lab011 {
+    public static void main(String[] args) {
+
+        //ESCAPE CHAR
+        char new_line='\n';
+        char tab_line='\t';
+        char back_space='\b';// delete last word of first letter
+        char car_r= '\r'; //delete the previous word
+        System.out.println("Balaji"+new_line+"Chandran");
+        System.out.println("Balaji"+tab_line+"Chandran");
+        System.out.println("Balaji"+back_space+"Chandran");
+        System.out.println("Balaji"+car_r+"Chandran");
+    }
+
+}
