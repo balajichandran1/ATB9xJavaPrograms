e3b1877310624469522fbbe7c7fd921e142a263e
BalajiChandran <catchbaalaaji@gmail.com>
2024-10-16 15:56:05 +0530

    Updating Task exercises of Oct16 2024

---
+++ src/October/ex_15102024/Lab017_PracticeRelationalOperator.java
@@ -0,0 +1,13 @@
+package October.ex_15102024;
+
+public class Lab017_PracticeRelationalOperator {
+    public static void main(String[] args) {
+        char age='2';
+        int a1 = 2;
+        System.out.println(age==a1);
+
+        int a=100, b=20;
+        boolean bool= true;
+        System.out.println((a>=b)==bool);
+    }
+}
