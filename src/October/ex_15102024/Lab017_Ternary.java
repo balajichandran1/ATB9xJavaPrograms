aa3494c3adf886240e2cd64645b81496117b20e2
BalajiChandran <catchbaalaaji@gmail.com>
2024-10-22 05:37:04 +0530

    Updating some of the Task exercises of Oct 21 2024

---
+++ src/October/ex_15102024/Lab017_Ternary.java
@@ -0,0 +1,10 @@
+package October.ex_15102024;
+
+public class Lab017_Ternary {
+    public static void main(String[] args) {
+        // - result = condition ? expression1 : expression2;
+        int number = -5;
+        String result = number > 0 ? "Positive" : "Negative";
+        System.out.println(result);
+    }
+}
---
+++ src/October/ex_15102024/Lab018_TernarySample2.java
@@ -0,0 +1,9 @@
+package October.ex_15102024;
+
+public class Lab018_TernarySample2 {
+    public static void main(String[] args) {
+        int age = 19;
+        String result = age > 18 ? "Allowed to Vote" : "Not Allowed to Vote";
+        System.out.println(result);
+    }
+}
